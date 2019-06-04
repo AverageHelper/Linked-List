19857d39d9dabb1d31fbf74a20036c8198f2cb9f
James Robinson <silverenderman@gmail.com>
2019-06-03 20:45:51 -0600

    Initial Commit

---
+++ Lab6-Linked List/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab6-Linked List
+//
+//  Created by James Robinson on 6/3/19.
+//  Copyright © 2019 James Robinson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
